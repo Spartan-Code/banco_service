1e81887f74b6d2b2921d8f667928b21d5b5bb944
German <you@example.com>
2015-12-19 20:06:57 +0100

    Usuario Implementado en banco_service

--- src/com/fpmislata/banco/business/service/impl/UsuarioServiceImpl.java
+++ src/com/fpmislata/banco/business/service/impl/UsuarioServiceImpl.java
@@ -7,7 +7,10 @@ package com.fpmislata.banco.business.service.impl;
 
 import com.fpmislata.banco.business.domain.Usuario;
 import com.fpmislata.banco.business.service.UsuarioService;
+import com.fpmislata.banco.persistence.dao.UsuarioDAO;
 import java.util.List;
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  *
@@ -15,6 +18,23 @@ import java.util.List;
  */
 public class UsuarioServiceImpl extends GenericServiceImpl<Usuario> implements UsuarioService {
 
+    @Autowired
+    UsuarioDAO usuarioDAO;
+
+    public UsuarioServiceImpl() {
+    }
+    
+    
+    
+
+    @PostConstruct 
+    public void asignarDAO() {
+        this.genericDAO = usuarioDAO;
+    }
+    
+    
+    
+    
     @Override
     public List<Usuario> findByNombre(String nombre) {
         return null;
