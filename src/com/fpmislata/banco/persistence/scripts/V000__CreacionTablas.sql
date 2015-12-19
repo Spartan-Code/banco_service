9f4920623ebb407f5f1d191b3be9049e25a0d24a
German <you@example.com>
2015-12-19 19:10:52 +0100

    Cambio Script sql

---
+++ src/com/fpmislata/banco/persistence/scripts/V000__CreacionTablas.sql
@@ -0,0 +1,23 @@
+CREATE TABLE `entidadbancaria` (
+	`idEntidadBancaria` INT(11) NOT NULL AUTO_INCREMENT,
+	`nombre` VARCHAR(50) NULL DEFAULT NULL,
+	`codigoEntidad` VARCHAR(50) NULL DEFAULT NULL,
+	`fechaCreacion` DATE NULL DEFAULT NULL,
+	`direccion` VARCHAR(100) NULL DEFAULT NULL,
+	`CIF` VARCHAR(50) NULL DEFAULT NULL,
+	PRIMARY KEY (`idEntidadBancaria`),
+	UNIQUE INDEX `codigoEntidad` (`codigoEntidad`)
+)
+ENGINE=InnoDB
+;
+
+CREATE TABLE `user` (
+	`idUsuario` INT(10) NOT NULL AUTO_INCREMENT,
+	`nombre` VARCHAR(50) NULL DEFAULT NULL,
+	`contraseña` VARCHAR(250) NULL DEFAULT NULL,
+	`email` VARCHAR(100) NULL DEFAULT NULL,
+	`rol` VARCHAR(50) NULL DEFAULT NULL,
+	PRIMARY KEY (`idUsuario`)
+)
+ENGINE=InnoDB
+;
